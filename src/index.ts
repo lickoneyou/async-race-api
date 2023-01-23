2c4eaca97a44533d139da5e1153fa37ef34f3964
Kiryll Khomchanka <dickdurex265@gmail.com>
2023-01-23 12:39:22 +0300

    feat: add start button

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { App } from './components/app'
 import { addCar, updateCar } from './components/cars/createCars'
 import { garageArray, removeCar } from './components/garage/garage'
 import { generateRandomCars } from './components/cars/generateCars'
+import { isConstructorDeclaration, JsxEmit } from 'typescript'
+import { node } from 'webpack'
 
 export const app = new App()
 export const body = document.querySelector('body') as HTMLBodyElement
@@ -44,22 +46,76 @@ const generateCars = document.querySelector(
 ) as HTMLButtonElement
 generateCars.addEventListener('click', generateRandomCars)
 
-
 const carSelect = document.querySelectorAll('.carSelect') as NodeListOf<Element>
 let carNumber: number = 0
 carSelect.forEach((carSelect) => {
   carSelect.addEventListener('click', (e) => {
-    let carIndex:string[] = [].filter(el => el != '')
-    garageArray.forEach((el) => carIndex.push(el.match(carSelect.id)?.input || ''))
+    let carIndex: string[] = [].filter((el) => el != '')
+    garageArray.forEach((el) =>
+      carIndex.push(el.match(carSelect.id)?.input || ''),
+    )
     carNumber = garageArray.indexOf(carIndex.join(''))
     console.log(carNumber)
   })
-  })
+})
 
-  const updateButton = document.querySelector('.updateButton') as HTMLButtonElement
+const updateButton = document.querySelector(
+  '.updateButton',
+) as HTMLButtonElement
 
-  updateButton.addEventListener('click', () =>{
-    garageArray[carNumber] = updateCar()
-    localStorage.setItem('garageArray',JSON.stringify(garageArray));
-location.reload()
+updateButton.addEventListener('click', () => {
+  garageArray[carNumber] = updateCar()
+  localStorage.setItem('garageArray', JSON.stringify(garageArray))
+  location.reload()
+})
+
+const server = 'http://localhost:3000/'
+
+type CarsEngine = {
+  velocity: number
+  distance: number
+}
+
+async function carsEngine() {
+  const carSpeedPromise = (
+    await fetch(`http://localhost:3000/engine?id=2&status=started`, {
+      method: 'PATCH',
+    })
+  )
+  const data: CarsEngine = await carSpeedPromise.json()
+   console.log(data)
+  return +(data.distance / data.velocity / 1000).toFixed(2)
+}
+
+async function driveMode() {
+  const drive = (await fetch(`http://localhost:3000/engine?id=2&status=drive`, {method: 'PATCH'})).json()
+  return drive
+}
+driveMode()
+
+const carImg = document.querySelectorAll('.carImg') as NodeListOf<Element>
+const start = document.querySelectorAll('.start') as NodeListOf<Element>
+
+
+
+
+
+async function draw(el: HTMLElement) {
+  let speed = 0
+await carsEngine().then(data => speed = data)
+  el.style.transform = `translate(${window.innerWidth - 230}px)`
+  el.style.transition=`${speed}s` 
+}
+
+
+start.forEach(el => {
+  el.addEventListener('click', ()=>{
+    let a = el.id.replace(/[^0-9]/g,'')
+    carImg.forEach(el => {
+      if(el.id == `Capa_${a}`){
+        draw(el.parentElement?.querySelector('.carImg')!)
+      }
+    })
+
   })
+})
